92d9998945f3da4d190847991357f3cea811bc76
andreacapodacqua <andrea.capodacqua@pal-robotics.com>
2024-07-04 14:46:33 +0200

    removed base_type check

--- tiago_2dnav/launch/tiago_nav_bringup.launch.py
+++ tiago_2dnav/launch/tiago_nav_bringup.launch.py
@@ -34,9 +34,6 @@ def navigation_bringup(context, *args, **kwargs):
     world_name = LaunchConfiguration("world_name").perform(context)
     base_type = LaunchConfiguration("base_type").perform(context)
 
-    if base_type != "omni_base":
-        base_type = "pmb2"
-
     tiago_2dnav = get_package_share_directory("tiago_2dnav")
     pal_maps = get_package_share_directory("pal_maps")
     nav2_bringup = get_package_share_directory("nav2_bringup")
